807a425513b02e35998a6501bf6d01db915a15f3
Adam Kent <Adam@Adams-MacBook-Pro-2.local>
2019-07-22 16:39:45 -0700

    afternoon

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,18 @@ import "./Header.css";
 export default class Header extends React.Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            toggleMenu: false
+        }
+        this.toggleMenuFunc = this.toggleMenuFunc.bind(this);
+    }
+
+    toggleMenuFunc(){
+        this.setState((prevState) => {
+            return {
+                toggleMenu: !prevState.toggleMenu
+            }
+        })
     }
 
     render(){
@@ -13,14 +24,24 @@ export default class Header extends React.Component {
                 <div className="logo">
                     <a href="#/page-top">Start Bootstrap</a>
                 </div>
-                <button> MENU </button>
-                <nav>
+                <button onClick={this.toggleMenuFunc}> MENU </button>
+                <nav className={this.state.toggleMenu ? "show" : ""}>
                     <ul>
-                        <li><a href="#/services">Services</a></li>
-                        <li><a href="#/portfolio">Portfolio</a></li>
-                        <li><a href="#/about">About</a></li>
-                        <li><a href="#/team">Team</a></li>
-                        <li><a href="#/contact">Contact</a></li>
+                        <li>
+                            <a href="#/services">Services</a>
+                        </li>
+                        <li>
+                            <a href="#/portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#/about">About</a>
+                        </li>
+                        <li>
+                            <a href="#/team">Team</a>
+                        </li>
+                        <li>
+                            <a href="#/contact">Contact</a>
+                        </li>
                     </ul>
                 </nav>
             </div>
